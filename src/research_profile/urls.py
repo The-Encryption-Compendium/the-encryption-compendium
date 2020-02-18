7619d4780daff1fe5569b629af4e13084ae12883
Pranav <48041393+prgu6170@users.noreply.github.com>
2020-02-17 21:33:28 -0700

    showing current profile in the profile section along with researchers entries

--- src/research_profile/urls.py
+++ src/research_profile/urls.py
@@ -3,6 +3,5 @@ from research_profile.views import *
 from django.urls import path, include
 
 urlpatterns = [
-    path("", research_profile, name="research profile"),
-    path("my_entries/", research_my_entries, name="research my entries"),
+    path("", researcher_profile, name="research profile"),
 ]
--- src/research_profile/views.py
+++ src/research_profile/views.py
@@ -2,19 +2,23 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 from research_assistant.models import CompendiumEntry, CompendiumEntryTag
+from research_assistant.models import User
 
 
 @login_required
 @require_http_methods(["GET"])
-def research_profile(request):
-    return render(request, "research_profile.html")
-
-
-@login_required
-@require_http_methods(["GET"])
-def research_my_entries(request):
+def researcher_profile(request):
+    context = {"info": {}, "entries": []}
     my_entries = CompendiumEntry.objects.filter(owner=request.user.id)
-    context = {"entries": []}
+    context["info"]["username"] = (
+        User.objects.values("username")
+        .filter(username=request.user)
+        .first()["username"]
+    )
+    context["info"]["email"] = (
+        User.objects.values("email").filter(username=request.user).first()["email"]
+    )
+    context["info"]["number_of_entries"] = my_entries.count()
     for entry in my_entries:
         entry_dict = {"id": entry.id, "url": entry.url, "title": entry.title}
         tags_query_set = entry.tags.all()
@@ -23,4 +27,4 @@ def research_my_entries(request):
             tags.append(tag.tagname)
         entry_dict["tags"] = ", ".join(tags)
         context["entries"].append(entry_dict)
-    return render(request, "research_my_entries.html", context=context)
+    return render(request, "research_profile.html", context)
