The-Encryption-Compendium/the-encryption-compendium

commit 0856ec2cbca2050951e3c509b0f496b80f502a2e

Display errors and success messages on the 'add new user' page after attempting to create a new token. Fixes #20.

src/templates/add_user.html
@@ -9,6 +9,22 @@
 <form method="POST">
   {% csrf_token %}
 
+  {% if success or form.non_field_errors %}
+  <span class="d-flex justify-content-center">
+    <div class="alert alert-success mx-auto">
+      Your invite was sent!
+    </div>
+  </span>
+  {% endif %}
+
+  {% if form.non_field_errors %}
+  <span class="d-flex justify-content-center">
+    <div class="alert alert-danger mx-auto" role="alert">
+      An error occurred: {{ form.non_field_errors }}
+    </div>
+  </span>
+  {% endif %}
+
   <div class="container-fluid float-left mb-4">
     <div class="row">
       {% for field in form %}
@@ -18,6 +34,7 @@
       <div class="col-6 mb-3">
         {{ field }}
       </div>
+      {% include "includes/field_errors.html" with errors=field.errors only %}
       <div class="w-100"></div>
       {% endfor %}
     </div>

src/research_assistant/tests/test_views.py
@@ -60,6 +60,10 @@ Tests for email verification and user signup
 
 @tag("auth")
 class AddNewUserTest(UnitTest):
+    """
+    Tests for the view to invite new users to join the site.
+    """
+
     def setUp(self):
         super().setUp(preauth=True)
         self.user.is_staff = True
@@ -91,6 +95,9 @@ class AddNewUserTest(UnitTest):
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].to, [self.new_user_email])
 
+        # The page should say that we successfuly added a new user.
+        self.assertIn("Your invite was sent!", response.content)
+
     def test_only_staff_users_can_add_new_users(self):
         ### Only users that are staff members can send email verification tokens
         # Set User to be non-staff, and log the client back in
@@ -120,6 +127,14 @@ class AddNewUserTest(UnitTest):
         response = self.client.post(reverse("add new user"), data)
         self.assertFalse(SignupToken.objects.filter(email=self.new_user_email).exists())
         self.assertNotIn(self.new_user_email, response.content.decode("utf-8"))
+
+    def test_cannot_send_token_when_outstanding_token_exists(self):
+        token = SignupToken.objects.create(email=self.new_user_email)
+        data = {"email": self.new_user_email, "create_user": ""}
+        response = self.client.post(reverse("add new user"), data)
+
+        self.assertEqual(len(SignupToken.objects.all()), 1)
+        self.assertIn("Signup token with this Email already exists.", response.content)
 
 
 class SignupNewUserTest(UnitTest):
@@ -179,7 +194,7 @@ Tests for user logout
 
 class LogoutTestCase(UnitTest):
     """
-    Tests for the logout view that logs a user out of the site.
+    Tests for the logout view.
     """
 
     def setUp(self):

src/research_assistant/forms.py
@@ -95,7 +95,7 @@ class AddNewUserForm(forms.ModelForm):
         # Email cannot already belong to a registered user
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError(
-                _("%(email)s is already registered with a user."),
+                _("This email address has already been registered."),
                 params={"email": email},
             )
 

src/research_assistant/tests/test_navigation.py
@@ -160,7 +160,6 @@ class InviteUserFunctionalTestCase(FunctionalTest):
         self.user.is_staff = True
         self.user.save()
 
-    @tag("tmp")
     def test_invite_new_user_to_site(self):
         # Check that database hasn't been modified in a way that would interfere
         # with the tests.

commit ac8a13925abd4cf6a776e5157aff3cdc61c295cb

Fix a couple of errors in the add user view that were introduced in 0856ec2cbca2050951e3c509b0f496b80f502a2e so that tests now pass and all success / error messages appear.

src/research_assistant/views.py
@@ -55,7 +55,12 @@ def research_logout(request):
 @user_passes_test(lambda user: user.is_staff)
 @require_http_methods(["GET", "POST"])
 def add_new_user(request):
+    """
+    An admin view that allows staff members to invite new researchers to create an
+    account on the site.
+    """
     form = AddNewUserForm(request.POST if request.POST else None)
+    created_user = False
     if request.POST and "create_user" in request.POST and form.is_valid():
         token = form.save()
         url = request.build_absolute_uri(token.signup_location)
@@ -65,6 +70,7 @@ def add_new_user(request):
             settings.EMAIL_HOST_USER,
             [form.cleaned_data["email"]],
         )
+        created_user = True
     elif request.POST and "del_email" in request.POST:
         delete_form = TokenDeleteForm({"email": request.POST["del_email"]})
         if delete_form.is_valid():
@@ -76,7 +82,11 @@ def add_new_user(request):
     return render(
         request,
         "add_user.html",
-        context={"form": form, "outstanding_tokens": outstanding_tokens},
+        context={
+            "form": form,
+            "outstanding_tokens": outstanding_tokens,
+            "created_user": created_user,
+        },
     )
 
 

src/templates/add_user.html
@@ -9,7 +9,7 @@
 <form method="POST">
   {% csrf_token %}
 
-  {% if success or form.non_field_errors %}
+  {% if created_user %}
   <span class="d-flex justify-content-center">
     <div class="alert alert-success mx-auto">
       Your invite was sent!
@@ -20,7 +20,7 @@
   {% if form.non_field_errors %}
   <span class="d-flex justify-content-center">
     <div class="alert alert-danger mx-auto" role="alert">
-      An error occurred: {{ form.non_field_errors }}
+      {{ form.non_field_errors }}
     </div>
   </span>
   {% endif %}

src/research_assistant/tests/test_views.py
@@ -96,7 +96,7 @@ class AddNewUserTest(UnitTest):
         self.assertEqual(mail.outbox[0].to, [self.new_user_email])
 
         # The page should say that we successfuly added a new user.
-        self.assertIn("Your invite was sent!", response.content)
+        self.assertIn("Your invite was sent!", response.content.decode("utf-8"))
 
     def test_only_staff_users_can_add_new_users(self):
         ### Only users that are staff members can send email verification tokens
@@ -134,7 +134,10 @@ class AddNewUserTest(UnitTest):
         response = self.client.post(reverse("add new user"), data)
 
         self.assertEqual(len(SignupToken.objects.all()), 1)
-        self.assertIn("Signup token with this Email already exists.", response.content)
+        self.assertIn(
+            "Signup token with this Email already exists.",
+            response.content.decode("utf-8"),
+        )
 
 
 class SignupNewUserTest(UnitTest):