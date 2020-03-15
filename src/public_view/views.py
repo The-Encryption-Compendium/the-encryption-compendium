70604524f2c6da452c7d743fe9c2003ba2a377d0
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-03-15 03:30:53 -0600

    Make the landing page look a little nicer. Ensure that the landing page actually shows the real tags.

--- src/public_view/views.py
+++ src/public_view/views.py
@@ -4,11 +4,14 @@ Views for the public-facing side of the site.
 
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from research_assistant.models import CompendiumEntryTag
 
 
 @require_http_methods(["GET"])
 def landing_page(request):
-    return render(request, "landing_page.html")
+    # Get a list of all of the tags, sorted alphabetically
+    tags = CompendiumEntryTag.objects.order_by("tagname").all()
+    return render(request, "landing_page.html", context={"tags": tags})
 
 
 @require_http_methods(["POST"])
