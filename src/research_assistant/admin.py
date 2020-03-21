fa3fb315a6b80b4949052457ed9d32096321ea24
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-03-21 02:54:22 -0600

    Move user admin forms into the users app.

--- src/research_assistant/admin.py
+++ src/research_assistant/admin.py
@@ -1,23 +1,10 @@
 from django.contrib import admin
 from research_assistant.models import CompendiumEntry
-from research_assistant.forms import UserAdminForm, CompendiumEntryForm
-from users.models import User
-
-"""
----------------------------------------------------
-Admin models
----------------------------------------------------
-"""
-
-
-class UserAdmin(admin.ModelAdmin):
-    form = UserAdminForm
+from research_assistant.forms import CompendiumEntryForm
 
 
 class CompendiumEntryAdmin(admin.ModelAdmin):
     form = CompendiumEntryForm
 
 
-# Register your models here.
-admin.site.register(User, UserAdmin)
 admin.site.register(CompendiumEntry, CompendiumEntryAdmin)
--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from users.forms import UserAdminForm
+from users.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    form = UserAdminForm
+
+
+admin.site.register(User, UserAdmin)
