5f353026039c54a97b5077dd528f250eb85bf612
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-03-29 04:30:01 -0600

    Configure the Apache Solr instance to query data from the Postgres database.

---
+++ deploy_tools/solr/setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+### Setup script to run before starting Solr
+
+# Create a data-config.xml with the database credentials
+export POSTGRES_USER=$POSTGRES_USER
+export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+DATA_CONFIG="${CONFIG_DIR}/data-config.xml"
+
+cat > "${DATA_CONFIG}" <<- EOXML
+<dataConfig>
+
+  <dataSource
+    type="JdbcDataSource"
+    name="compendium-db"
+    url="jdbc:postgresql://tec-database/$POSTGRES_DB"
+    driver="org.postgresql.Driver"
+    readOnly="true"
+    user="$POSTGRES_USER"
+    password="$POSTGRES_PASSWORD" />
+
+  <document name="entries">
+    <entity name="compendium" query="select title, abstract from compendium" />
+  </document>
+
+</dataConfig>
+EOXML
+
+echo "${DATA_CONFIG} written"
