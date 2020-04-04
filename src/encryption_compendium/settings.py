4a472441f33d2a093bbf5e7d8ac6fcb346ebd63a
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2020-04-03 22:23:29 -0600

    Move static files directory in the Gunicorn and Nginx containers.

--- src/encryption_compendium/settings.py
+++ src/encryption_compendium/settings.py
@@ -215,7 +215,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, os.pardir, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets")]
 
 STATICFILES_FINDERS = (
